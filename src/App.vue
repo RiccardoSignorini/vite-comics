d109de7ec25545c9adea9d9583e6d3eb172566cc
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-05-03 15:00:22 +0200

    apertura cartella lavoro

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  import HelloWorld from "./components/HelloWorld.vue"
+
+  export default{
+    name: "App",
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+
+<template>
+  <div class="container text-center">
+    <h1>Hello world!</h1>
+  </div>
+  <HelloWorld/>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+  export default{
+      name: "HelloWorld",
+    }
+</script>
+
+<template>
+  <div class="container text-center">
+    <h2>La mia seconda app con Vite!</h2>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
